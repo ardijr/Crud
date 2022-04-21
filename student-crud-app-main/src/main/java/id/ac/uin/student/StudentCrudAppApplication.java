bfb4884d56ec5402bb530ff89189813acfe4cbfe
ardijr <101171711+ardijr@users.noreply.github.com>
2022-04-21 21:38:10 +0700

    CRUDAPI

---
+++ student-crud-app-main/src/main/java/id/ac/uin/student/StudentCrudAppApplication.java
@@ -0,0 +1,14 @@
+package id.ac.uin.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(StudentCrudAppApplication.class, args);
+    }
+
+}
